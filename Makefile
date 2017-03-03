952ea10f2105d9ab7f41c5ddf3a151a9b8ed5eb0
Tawsif Azad <tawsifazad@gmail.com>
2017-03-03 12:41:26 -0600

    Create black sfml window

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: rpg
 
 rpg: bin/main.o
-	g++ bin/main.o -o rpg
+	g++ bin/main.o -o rpg -lsfml-graphics -lsfml-window -lsfml-system
 bin/main.o: src/main.cpp
 	g++ -c -g -Wall -Wextra -std=c++11 src/main.cpp -o bin/main.o
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,18 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-#define WIDTH 40
-#define HEIGHT 20
-#define GRASS '~'
-#define PLAYER 'p'
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800,600), "My window");
 
-void buildMap (char (&map) [HEIGHT][WIDTH]) {
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            map[i][j] = GRASS;
-        }
-    }
-}
-
-void printMap (char (&map) [HEIGHT][WIDTH], int playerx, int playery) {
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            if (i == playery && j == playerx) {
-                std::cout << PLAYER;
-            } else {
-                std::cout << map[i][j];
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
             }
         }
-        std::cout << std::endl;
+        window.clear(sf::Color::Black);
+        window.display();
     }
-}
 
-int main () {
-    char map[HEIGHT][WIDTH];
-    int playerx = WIDTH/2;
-    int playery = HEIGHT/2;
-    buildMap(map);
-    printMap(map, playerx, playery);
-    while (1) {
-        // TODO implement player movement
-    }
     return 0;
 }
