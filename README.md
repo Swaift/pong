79b9d6d8c9173bfee5e78689e0a9b70ba6b3246d
Tawsif Azad <tawsifazad@gmail.com>
2017-03-05 00:41:34 -0600

    Refactor into state machine, improve AI

---
+++ README.md
@@ -0,0 +1,9 @@
+# SFML Demo Games
+
+## Pong
+- [  ] Line dividing player areas
+- [  ] Splash screen
+- [  ] Restart game after each point
+- [  ] Display score on play screen
+- [  ] Game over screen with win/loss (first to 5 points wins)
+- [  ] ~~Make ball and AI paddle get faster over time~~
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,110 @@
 #include <SFML/Graphics.hpp>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
 
-#define PADDLE_WIDTH 20
-#define PADDLE_HEIGHT 80
+#define PADDLE_THICKNESS 20
+#define PADDLE_LENGTH 80
 #define BALL_RADIUS 10
+#define WALL_THICKNESS 20
+#define PADDLE_SPACE 20
 
-#define PADDLE_STEP 4
-#define BALL_STEP 8
-#define DOWN PADDLE_STEP
-#define UP -PADDLE_STEP
+#define MAX_BALL_OFFSET (PADDLE_LENGTH/2 + BALL_RADIUS)
+#define R_PADDLE_ACCEL 7 // lower is more acceleration
+#define BALL_STEP 10
+
+float getPaddleCenter(sf::Shape& paddle) {
+    return paddle.getPosition().y + PADDLE_LENGTH/2;
+}
+
+float getBallCenter(sf::Shape& ball) {
+    return ball.getPosition().y + BALL_RADIUS;
+}
+
+float getBallOffset(sf::Shape& ball, sf::Shape& paddle) {
+    return getBallCenter(ball) - getPaddleCenter(paddle);
+}
+
+void processPlay(sf::RenderWindow& window,
+                 sf::Shape& topWall,
+                 sf::Shape& bottomWall,
+                 sf::Shape& lPaddle,
+                 sf::Shape& rPaddle,
+                 sf::Shape& ball,
+                 float& ballDx,
+                 float& ballDy)
+{
+    // rPaddle AI
+    if (std::fabs(getBallOffset(ball, rPaddle)) > PADDLE_LENGTH/2)
+    {
+        rPaddle.move(0, getBallOffset(ball, rPaddle)/R_PADDLE_ACCEL);
+    }
+
+    // player paddle y-position same as mouse unless out of screen
+    lPaddle.setPosition(lPaddle.getPosition().x, sf::Mouse::getPosition(window).y - PADDLE_LENGTH/2);
+    if (lPaddle.getPosition().y < topWall.getPosition().y + WALL_THICKNESS) {
+        lPaddle.setPosition(lPaddle.getPosition().x, topWall.getPosition().y + WALL_THICKNESS);
+    } else if (lPaddle.getPosition().y + PADDLE_LENGTH > bottomWall.getPosition().y) {
+        lPaddle.setPosition(lPaddle.getPosition().x, bottomWall.getPosition().y - PADDLE_LENGTH);
+    }
+
+    // bounce ball off walls
+    if (ball.getGlobalBounds().intersects(topWall.getGlobalBounds())) {
+        ballDy = std::fabs(ballDy);
+    } else if (ball.getGlobalBounds().intersects(bottomWall.getGlobalBounds())) {
+        ballDy = -std::fabs(ballDy);
+    }
+    // bounce ball off paddles
+    if (ball.getGlobalBounds().intersects(lPaddle.getGlobalBounds())) {
+        ballDy = getBallOffset(ball, lPaddle)/MAX_BALL_OFFSET * BALL_STEP;
+        ballDx = -ballDx;
+    } else if (ball.getGlobalBounds().intersects(rPaddle.getGlobalBounds())) {
+        ballDy = getBallOffset(ball, rPaddle)/MAX_BALL_OFFSET * BALL_STEP;
+        ballDx = -ballDx;
+    }
+    ball.move(ballDx, ballDy);
+
+    window.draw(topWall);
+    window.draw(bottomWall);
+    window.draw(lPaddle);
+    window.draw(rPaddle);
+    window.draw(ball);
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH,WINDOW_HEIGHT), "My window");
-    srand((int) time(0));
-    int gameOver = 0;
 
-    // game over screen
-    // TODO
+    // initialize state machine
+    enum class State {
+        SPLASH,
+        PLAY,
+        GAMEOVER
+    };
+    State currentState = State::PLAY;
+    State nextState = State::PLAY;
+    
+    // create top and bottom walls
+    sf::RectangleShape topWall(sf::Vector2f(WINDOW_WIDTH, WALL_THICKNESS));
+    sf::RectangleShape bottomWall(sf::Vector2f(WINDOW_WIDTH, WALL_THICKNESS));
+    topWall.setPosition(0, 0);
+    bottomWall.setPosition(0, WINDOW_HEIGHT - WALL_THICKNESS);
 
-    // calculate paddle coordinates based on top left corner
-    sf::RectangleShape lPaddle(sf::Vector2f(PADDLE_WIDTH,PADDLE_HEIGHT));
-    lPaddle.setPosition(20, WINDOW_HEIGHT/2 - PADDLE_HEIGHT/2);
-    sf::RectangleShape rPaddle(sf::Vector2f(PADDLE_WIDTH,PADDLE_HEIGHT));
-    rPaddle.setPosition(WINDOW_WIDTH - PADDLE_WIDTH - 20, WINDOW_HEIGHT/2 - PADDLE_HEIGHT/2);
+    // create paddles
+    sf::RectangleShape lPaddle(sf::Vector2f(PADDLE_THICKNESS, PADDLE_LENGTH));
+    sf::RectangleShape rPaddle(sf::Vector2f(PADDLE_THICKNESS, PADDLE_LENGTH));
+    lPaddle.setPosition(PADDLE_SPACE, WINDOW_HEIGHT/2 - PADDLE_LENGTH/2);
+    rPaddle.setPosition(WINDOW_WIDTH - PADDLE_THICKNESS - PADDLE_SPACE, WINDOW_HEIGHT/2 - PADDLE_LENGTH/2);
 
+    // place ball randomly along center axis
+    srand((int) time(NULL));
     sf::CircleShape ball(BALL_RADIUS);
-    int randomBallPos = (rand() % WINDOW_HEIGHT) + 1;
-    int balldx, balldy;
-    do {
-        balldx = (rand() % BALL_STEP * 2) + 1 - BALL_STEP;
-        balldy = (rand() % BALL_STEP * 2) + 1 - BALL_STEP;
-    } while (abs(balldx) <= 1 || abs(balldy) <= 1);
-    ball.setPosition(WINDOW_WIDTH/2 - BALL_RADIUS/2, randomBallPos);
-
-    int rPaddleDir = DOWN;
+    ball.setPosition(WINDOW_WIDTH/2 - BALL_RADIUS/2, rand() % (WINDOW_HEIGHT - WALL_THICKNESS * 2 - BALL_RADIUS * 2) + WALL_THICKNESS);
+    float ballDx = -BALL_STEP;
+    // ballDy can either be -BALL_STEP/2, 0, or BALL_STEP/2
+    float ballDy = (rand() % 3 - 1) * BALL_STEP/2;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -46,47 +113,31 @@ int main() {
                 window.close();
             }
         }
-
-        if (rPaddle.getGlobalBounds().top + PADDLE_HEIGHT/2 < ball.getGlobalBounds().top + BALL_RADIUS &&
-                rPaddle.getGlobalBounds().top + PADDLE_HEIGHT < WINDOW_HEIGHT)
-        {
-            rPaddle.move(0, DOWN);
-        } else if (rPaddle.getGlobalBounds().top + PADDLE_HEIGHT/2 > ball.getGlobalBounds().top + BALL_RADIUS &&
-                rPaddle.getGlobalBounds().top > 0)
-        {
-            rPaddle.move(0, UP);
-        }
-
-        // player paddle controls
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) &&
-                lPaddle.getGlobalBounds().top > 0)
-        {
-            lPaddle.move(0, UP);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
-                lPaddle.getGlobalBounds().top + PADDLE_HEIGHT < WINDOW_HEIGHT)
-        {
-            lPaddle.move(0, DOWN);
-        }
-
-        // move ball
-        if ((ball.getGlobalBounds().top <= 0 && balldy < 0) ||
-            (ball.getGlobalBounds().top + BALL_RADIUS * 2 >= WINDOW_HEIGHT && balldy > 0)) {
-            balldy = -balldy;
-        }
-        if (ball.getGlobalBounds().intersects(lPaddle.getGlobalBounds()) ||
-            ball.getGlobalBounds().intersects(rPaddle.getGlobalBounds())) {
-            balldx = -balldx;
-        }
-        ball.move(balldx, balldy);
-
         window.clear(sf::Color::Black);
-        if (!gameOver) {
-            window.draw(lPaddle);
-            window.draw(rPaddle);
-            window.draw(ball);
-        } else {
-            //window.draw(scoreScreen);
+
+        currentState = nextState;
+        switch (currentState) {
+            case State::SPLASH:
+                // TODO
+                nextState = State::SPLASH;
+                break;
+            case State::PLAY:
+                processPlay(window,
+                            topWall,
+                            bottomWall,
+                            lPaddle,
+                            rPaddle,
+                            ball,
+                            ballDx,
+                            ballDy);
+                nextState = State::PLAY;
+                break;
+            case State::GAMEOVER:
+                // TODO
+                nextState = State::GAMEOVER;
+                break;
         }
+
         window.display();
     }
 
