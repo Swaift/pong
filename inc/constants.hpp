4eec8e60af3a12f4414a4ffec77d6d4d0bc69928
Tawsif Azad <tawsifazad@gmail.com>
2017-03-11 07:47:00 -0600

    Add back renamed/moved files

---
+++ inc/constants.hpp
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 800;
+
+const int WALL_THICKNESS = 20;
+
+const int PADDLE_THICKNESS = 20;
+const int PADDLE_LENGTH = 80;
+const int R_PADDLE_ACCEL = 7; // lower is more acceleration
+
+const int BALL_RADIUS = 10;
+const int BALL_STEP = 10;
+const int MAX_BALL_OFFSET = PADDLE_LENGTH/2 + BALL_RADIUS;
+
+const int SPACING = 20;
+const int FONT_SIZE = 100;
+const int WAIT_TIME = 100;
+
+#endif // CONSTANTS_HPP
