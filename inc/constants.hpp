c7f28c4af5f5d50e6abeb7b3cc9a8506a81dd3f2
Tawsif Azad <tawsifazad@gmail.com>
2017-03-13 17:33:31 -0500

    Adjust constants to make gameplay faster-paced

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -8,13 +8,13 @@ const int WALL_THICKNESS = 20;
 
 const int PADDLE_THICKNESS = 20;
 const int PADDLE_LENGTH = 80;
-const int R_PADDLE_ACCEL = 7; // lower is more acceleration
+const int R_PADDLE_ACCEL = 4; // lower is more acceleration
 
 const int BALL_RADIUS = 10;
-const int BALL_STEP = 10;
+const int BALL_STEP = 25;
 const int MAX_BALL_OFFSET = PADDLE_LENGTH/2 + BALL_RADIUS;
 
 const int SPACING = 20;
-const int WAIT_TIME = 100;
+const int WAIT_TIME = 30;
 
 #endif // CONSTANTS_HPP
