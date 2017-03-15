9ead643429e22f7e33dcc5a559b48c4b41d7bba9
Tawsif Azad <tawsifazad@gmail.com>
2017-03-15 12:33:23 -0500

    Adjust constants to make gameplay slightly easier

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -8,13 +8,13 @@ const int WALL_THICKNESS = 20;
 
 const int PADDLE_THICKNESS = 20;
 const int PADDLE_LENGTH = 80;
-const int R_PADDLE_ACCEL = 4; // lower is more acceleration
+const int R_PADDLE_ACCEL = 5; // lower is more acceleration
 
 const int BALL_RADIUS = 10;
-const int BALL_STEP = 25;
+const int BALL_STEP = 15;
 const int MAX_BALL_OFFSET = PADDLE_LENGTH/2 + BALL_RADIUS;
 
 const int SPACING = 20;
-const int WAIT_TIME = 30;
+const int WAIT_TIME = 60;
 
 #endif // CONSTANTS_HPP
